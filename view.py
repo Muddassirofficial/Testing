3f0e80c8d06e307751a394e9f0e76a900f9c6b1f
Muddassir Farooq <135708579+Muddassirofficial@users.noreply.github.com>
2024-07-23 20:23:26 +0500

    Update view.py

--- view.py
+++ view.py
@@ -1,1 +1,17 @@
-
+def desk_view(page):
+    load_visitor_table(vb_table1)
+    print("Adding Desk view")
+    
+    page.views.append(
+        ft.View(
+            "/visitor_book",
+            [
+                 ft.AppBar(title=ft.Text("FIMS Management System"), bgcolor=ft.colors.BLUE_900,color='white'),
+                 vb_box1,vb_table
+            ],
+            drawer=navbar,
+            padding=10,bgcolor='white',horizontal_alignment='center',scroll=True
+        )
+    )
+    page.update()
+    return page.views
