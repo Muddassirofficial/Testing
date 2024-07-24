c4f65ce6230b7333739021f8f30bd08c8871c68f
SAMIANAWAZ <samiaturk#gmail.com>
2024-07-24 00:02:31 -0700

    changes in main

--- main.py
+++ main.py
@@ -1,64 +1,31 @@
-
 import flet as ft
-def main(page: ft.Page): 
-    page.theme_mode="light"
-    name = ft.TextField(label="Name", hint_text="Enter your name", width=300,  )
-    father_name = ft.TextField(label="F_Name", hint_text="Enter Father name", width=300,)
-    clas = ft.TextField(label="Class", width=300,)
-    btn = ft.ElevatedButton("Login", on_click=lambda _: page.go("/DATABASE"))
+from view import (register)
+
+from widget import set_global_page
+
+def route_change(route_event, page):
+    route = route_event.route
+    print("Route change:", route)
+    if route == "/":
+        print("Hello")
+        register(page)
     
-    box = ft.Container(
-        content=ft.Column(
-            [
-                name,
-                father_name,
-                clas,
-                btn
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        ),
-        padding=20,
-        border_radius=10,
-        bgcolor=ft.colors.GREY,
-        width=350,
-        height=300,
-        alignment=ft.alignment.center
-    )
+    
+    print("Current views:", page.views)
+    page.update()
 
-    def route_change(route):
-        page.views.clear()
-        page.views.append(
-            ft.View(
-                "/",
-                [
-                    ft.AppBar(title=ft.Text("Login"), bgcolor="blue" ),
-                    ft.Container(
-                        content=box,
-                        alignment=ft.alignment.center,
-                        expand=True
-                    ),
-                ]
-            )
-        )
-        if page.route == "/DATABASE":
-            page.views.append(
-                ft.View(
-                    "/DATABASE",
-                    [
-                        ft.AppBar(title=ft.Text("DATABASE"), bgcolor=ft.colors.SURFACE_VARIANT),
-                    ]
-                )
-            )
-        page.update()
+def view_pop(view, page):
+    page.views.pop()
+    top_view = page.views[-1]
+    page.go(top_view.route)
 
-    def view_pop(view):
-        page.views.pop()
-        top_view = page.views[-1]
-        page.go(top_view.route)
-
-    page.on_route_change = route_change
-    page.on_view_pop = view_pop
+def main(page: ft.Page):
+    page.title = "Routes Example"
+    set_global_page(page)
+    #page.window_width = 370
+    page.on_route_change = lambda route: route_change(route, page)
+    page.on_view_pop = lambda view: view_pop(view, page)
     page.go(page.route)
+    page.theme_mode = 'dark'
 
-ft.app(target=main)
+ft.app(target=main,view=ft.WEB_BROWSER)
